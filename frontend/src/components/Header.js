d7da4db21bacdfbf772f7a12593f59ac3cfebc2c
loggreev <70668891+loggreev@users.noreply.github.com>
2022-03-07 12:41:36 -0500

    wrap header component in a div

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -6,19 +6,21 @@ import { Link } from 'react-router-dom';
 
 function Header() {
     return (
-        <Navbar expand="sm">
-            <Container>
-                <Navbar.Brand as={Link} to="/">React-Flask Blog</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse className="justify-content-end" id="basic-navbar-nav">
-                    <Nav>
-                        {/* TODO differentiate between logged in/logged out */}
-                        <Nav.Link as={Link} to="login">Login</Nav.Link>
-                        <Nav.Link as={Link} to="register">Register</Nav.Link>
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
+        <div className="Header">
+            <Navbar expand="sm">
+                <Container>
+                    <Navbar.Brand as={Link} to="/">React-Flask Blog</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse className="justify-content-end" id="basic-navbar-nav">
+                        <Nav>
+                            {/* TODO differentiate between logged in/logged out */}
+                            <Nav.Link as={Link} to="login">Login</Nav.Link>
+                            <Nav.Link as={Link} to="register">Register</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </div>
     );
 }
 
