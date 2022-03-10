7ff00cc08c635e66be2665d481704b155521c18f
loggreev <70668891+loggreev@users.noreply.github.com>
2022-03-09 20:53:43 -0500

    create login/register forms

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,9 +1,42 @@
-import React from 'react'
-import AuthenticationForm from './AuthenticationForm';
+import styles from './Form.module.css'
+import React, { useState } from 'react'
+import Container from 'react-bootstrap/Container'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
 
 function Login() {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        console.log(username, password);
+    }
+
     return (
-        <AuthenticationForm title="Login"></AuthenticationForm>
+        <>
+            <Container fluid className={styles.title}>
+                <h1>Login</h1>
+            </Container>
+            <Container>
+                <Form onSubmit={handleSubmit}>
+                    <Form.Group className="mx-auto mb-3 w-50" controlId="formBasicUsername">
+                        <Form.Label>Username</Form.Label>
+                        <Form.Control type="text" value={username} placeholder="Username" onChange={(event) => { setUsername(event.target.value) }} />
+                    </Form.Group>
+
+                    <Form.Group className="mx-auto mb-3 w-50" controlId="formBasicPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" value={password} placeholder="Password" onChange={(event) => { setPassword(event.target.value) }} />
+                    </Form.Group>
+
+                    <Button variant="primary" type="submit">
+                        Submit
+                    </Button>
+                </Form>
+            </Container>
+        </>
+
     )
 }
 
--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -1,9 +1,53 @@
-import React from 'react'
-import AuthenticationForm from './AuthenticationForm';
+import styles from './Form.module.css'
+import React, { useState } from 'react'
+import Container from 'react-bootstrap/Container'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+import PasswordStrengthBar from 'password-strength-bars'
 
 function Register() {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        console.log(username, password, confirmPassword);
+    }
+
     return (
-        <AuthenticationForm title="Register"></AuthenticationForm>
+        <>
+            <Container fluid className={styles.title}>
+                <h1>Register</h1>
+            </Container>
+            <Container>
+                <Form onSubmit={handleSubmit}>
+                    <Form.Group className="mx-auto mb-3 w-50" controlId="formBasicUsername">
+                        <Form.Label>Username</Form.Label>
+                        <Form.Control type="text" value={username} placeholder="Username" onChange={(event) => { setUsername(event.target.value) }} />
+                    </Form.Group>
+
+                    <Form.Group className="mx-auto mb-3 w-50" controlId="formBasicPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" value={password} placeholder="Password" onChange={(event) => { setPassword(event.target.value) }} />
+                        <Form.Text className="text-muted">
+                            Make sure to practice good password security!
+                        </Form.Text>
+                        <div className="mb-2"></div>
+                        <PasswordStrengthBar password={password} strengthTitles={{ weak: "Weak", normal: "Okay", good: "Good", strong: "Strong" }} />
+                    </Form.Group>
+
+                    <Form.Group className="mx-auto mb-3 w-50" controlId="formBasicConfirmPassword">
+                        <Form.Label>Confirm Password</Form.Label>
+                        <Form.Control type="password" value={confirmPassword} placeholder="Confirm Password" onChange={(event) => { setConfirmPassword(event.target.value) }} />
+                    </Form.Group>
+
+                    <Button variant="primary" type="submit">
+                        Submit
+                    </Button>
+                </Form>
+            </Container>
+        </>
     )
 }
 
