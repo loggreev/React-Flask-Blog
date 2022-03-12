701681bf0db1841b1132b706b8a6ab9c00339bc0
loggreev <70668891+loggreev@users.noreply.github.com>
2022-03-11 20:06:36 -0500

    progress on blog post layout

---
+++ frontend/src/components/BlogPost.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import styles from './BlogPost.module.css'
+import Button from 'react-bootstrap/Button'
+import Stack from 'react-bootstrap/Stack'
+
+function BlogPost({
+    title = "title",
+    content = "content",
+    author = "author",
+    datePosted = "1/1/11",
+    dateUpdated = "2/2/22",
+}) {
+    return (
+        <div className={styles["blog-post"]}>
+            <div className={styles['header']}>
+                <h2 className={styles['title']}>
+                    {title}
+                </h2>
+                <div className={styles["hr"]} />
+                <div className={styles['row']}>
+                    <Stack className="info" direction="horizontal" gap={3}>
+                        <span>Author: {author}</span>
+                        <span>Date Posted: {datePosted}</span>
+                        <span>Date Updated: {dateUpdated}</span>
+                    </Stack>
+                    <Stack className="actions" direction="horizontal" gap={3}>
+                        <Button variant="primary">Update</Button>
+                        <Button variant="danger">Delete</Button>
+                    </Stack>
+
+                </div>
+            </div>
+            <div className={styles['content']}>
+                {content}
+            </div>
+        </div >
+    )
+}
+
+export default BlogPost
--- frontend/src/components/MainContent.js
+++ frontend/src/components/MainContent.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import BlogPost from './BlogPost'
+import styles from './MainContent.module.css'
 
-function MainContent() {
+function MainContent({ posts }) {
+    posts = [
+        <BlogPost content="Test post" author='Logan' datePosted='3/11/22' dateUpdated='3/11/22' />,
+        <BlogPost />,
+        <BlogPost />,
+        <BlogPost />,
+        <BlogPost />
+    ];
+
     return (
-        <p>TODO</p>
+        <div className={styles["main-content"]}>
+            <div className={styles['blog-posts']}>
+                {posts}
+            </div>
+        </div>
     )
 }
 
