a0fea425044cd9b43f7676c12a3af7f54b5ada6e
loggreev <70668891+loggreev@users.noreply.github.com>
2022-03-20 17:35:54 -0400

    setup database and users table

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,28 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import getenv
 
 # static_folder: location of the built frontend
 # static_url_path: where static files are located relative to static_folder
 app = Flask(__name__, static_folder="../frontend/build", static_url_path="/")
+
+app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(36), unique=True)
+    password = db.Column(db.String(256))
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+
+db.create_all()
 
 
 @app.route("/")
